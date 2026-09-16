cd3a84f16ee0d6d241c374e84e97f54ddb1be93d
fer-idk <vm24031@ues.edu.sv>
2026-09-16 13:18:16 -0600

    Recopilando la información

--- proyectos/index.php
+++ proyectos/index.php
@@ -6,7 +6,27 @@ $_SESSION["datos"] ??=[];
 $TIPO_DE_EQUIPO = ["Laptop", "Desktop", "Telefono", "Reloj"];
 $MARCAS = ["SAMSUMG", "HP", "ASUS", "APPLE"];
 
-//Empezando con el arreglo asociativo
+//Empezando con el arreglo de los distintos diagnosticos (EL TIEMPO ESTIMADO SERA TOMADO EN HORAS)
+$SERVICIOS_DISPONIBLES = [
+    "limpieza" => ["codigo" => "1", "nombre" => "Limpieza interna", "categoria" => "hardware", "precio" => 15.00, "tiempo" => 1.5],
+    "sistema" => ["codigo" => "2", "nombre" => "Instalación de sistema operativo", "categoria" => "sotware", "precio" => 10.00, "tiempo" => 1.0],
+    "malware" => ["codigo" => "3", "nombre" => "Eliminacion de malware", "categoria" => "sotware", "precio" => 10.00, "tiempo" => 1.5],
+    "respaldo" => ["codigo" => "4", "nombre" => "Respaldo de datos", "categoria" => "sotware", "precio" => 20.00, "tiempo" => 2.5],
+    "componentes" => ["codigo" => "5", "nombre" => "Cambio de componentes", "categoria" => "hardware", "precio" => 25.00, "tiempo" => 0.5],
+    "mantenimiento" => ["codigo" => "6", "nombre" => "Mantenimiento preventivo", "categoria" => "hardware", "precio" => 25.00, "tiempo" => 1.0],
+];
+
+$errores = [];
+if($_SERVER["REQUEST_METHOD"]){
+    if(empty($_POST["nombre"]) || empty($_POST["correo"]) || empty($_POST["descripcion"])){
+        $errores[] = "Por favor rellene todos los parametros";
+    }
+    if(!empty($errores)){
+        $_SESSION["datos"][] = [
+            "nombre" => $_POST["nombre"],
+        ];
+    }
+}
 
 ?>
 
@@ -18,25 +38,39 @@ $MARCAS = ["SAMSUMG", "HP", "ASUS", "APPLE"];
     <title>Document</title>
 </head>
 <body>
+    <h1>Taller Fernando Melendez</h1>
     <div class="container-form">
         <form action="" method="post">
+            <h2>Ingrese la información del equipo:</h2><p>(Todos los campos son obligatorios)</p>
             <label for="">Nombre:</label>
             <input type="text" name="nombre">
             <label for="">Correo</label>
             <input type="email" name="correo">
+            <label for="">Tipo de dispositivo:</label>
             <select name="tipo_dispositivo" id="">
                 <?php foreach($TIPO_DE_EQUIPO as $value): ?>
                 <option value="<?= $value ?>"><?= $value ?></option>
                 <?php endforeach ?>
             </select>
+            <label for="">Seleccione marca:</label>
             <select name="marca_seleccionada" id="">
                 <?php foreach($MARCAS as $value): ?>
                     <option value="<?= $value ?>"><?= $value ?></option>
                 <?php endforeach; ?>
             </select>
             <label for="">Descipción del problema:</label>
-            <input type="text" name="descripcion">            
+            <input type="text" name="descripcion">     
+            <label for="">Servicios disponibles:</label>       
+            <select name="servicio" id="">
+                <?php foreach($SERVICIOS_DISPONIBLES as $key => $value): ?>
+                    <option value="<?= $key ?>"><?= $value["nombre"] ?></option>
+                <?php endforeach; ?>
+            </select>
+            <input type="submit">
         </form>
+    </div>
+    <div class="container-info">
+        
     </div>
 </body>
 </html>
